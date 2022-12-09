0c4bc22114cc1feabc75bf508142e2e7f142d97f
yhlee2 <yhlee2@rsupport.com>
2022-12-09 10:43:32 +0900

    생명주기 추가

--- app/src/main/java/com/example/broadcastreceiverexample/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreceiverexample/MainActivity.kt
@@ -12,25 +12,38 @@ import android.widget.TextView
 import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+
+    private val br = BroadcastReceiver()
+    val filter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+    val filter3 = IntentFilter(Intent.ACTION_POWER_CONNECTED)
+    val filter4 = IntentFilter(Intent.ACTION_POWER_DISCONNECTED)
+    val filter2 = IntentFilter("hello")
     @RequiresApi(Build.VERSION_CODES.O)
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val br = BroadcastReceiver()
-        val filter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
-        val filter3 = IntentFilter(Intent.ACTION_POWER_CONNECTED)
-        val filter4 = IntentFilter(Intent.ACTION_POWER_DISCONNECTED)
-        val filter2 = IntentFilter("hello")
         //충전기 꼽았다 뺏다하면 반응함
-        registerReceiver(br, filter)
-        registerReceiver(br, filter2)
-        registerReceiver(br, filter3)
-        registerReceiver(br, filter4)
         val button = findViewById<TextView>(R.id.hello)
         button.setOnClickListener {
             sendBroadcast(Intent("hello"))
         }
-        startForegroundService(Intent(this, Service::class.java))
+        //startForegroundService(Intent(this, Service::class.java))
+    }
+
+    override fun onStart() {
+        registerReceiver(br, filter)
+        registerReceiver(br, filter2)
+        registerReceiver(br, filter3)
+        registerReceiver(br, filter4)
+        super.onStart()
+    }
+    override fun onPause() {
+        unregisterReceiver(br)
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(br)
+        super.onDestroy()
     }
 }
