6899b42c5def8581ac91a8c15e0dec1eccbe83d7
yhlee2 <yhlee2@rsupport.com>
2022-12-09 09:54:37 +0900

    업데이트 1

--- app/src/main/java/com/example/broadcastreceiverexample/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreceiverexample/MainActivity.kt
@@ -16,10 +16,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val br = BroadcastReceiver()
         val filter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+        val filter3 = IntentFilter(Intent.ACTION_POWER_CONNECTED)
+        val filter4 = IntentFilter(Intent.ACTION_POWER_DISCONNECTED)
         val filter2 = IntentFilter("hello")
         //충전기 꼽았다 뺏다하면 반응함
         registerReceiver(br, filter)
         registerReceiver(br, filter2)
+        registerReceiver(br, filter3)
+        registerReceiver(br, filter4)
         val button = findViewById<TextView>(R.id.hello)
         button.setOnClickListener {
             sendBroadcast(Intent("hello"))
